ed18c84ae1d663f66d8f205dbb683aee0f9d9bc5
andrewacheampong <gorgi.andy@gmail.com>
2026-07-08 20:10:01 -0400

    feat: add config.sh

---
+++ config.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Cluster identity
+CLUSTER_NAME="homelab-k3s"
+EXPECTED_NODE_COUNT=3
+
+# Network
+INGRESS_NAMESPACE="nginx-ingress"
+INGRESS_SERVICE="ingress-nginx-controller"
+EXPECTED_INGRESS_IP="192.168.50.240"
+
+# NAS / NFS
+NAS_SERVER="192.168.50.227"
+NAS_DOWNLOADS_PATH="/downloads"
+NAS_MEDIA_PATH="/media"
+
+# Apps
+HOMEPAGE_NAMESPACE="homepage"
+HOMEPAGE_DEPLOYMENT="homepage"
+HOMEPAGE_URL="https://homepage.dev-andrew.com"
+
+GRAFANA_NAMESPACE="monitoring"
+GRAFANA_DEPLOYMENT="kube-prometheus-stack-grafana"
+GRAFANA_URL="https://grafana.dev-andrew.com"
+
+JELLYFIN_NAMESPACE="jellyfin"
+JELLYFIN_DEPLOYMENT="jellyfin"
+JELLYFIN_URL="https://jellyfin.dev-andrew.com"
+
+QBITTORRENT_NAMESPACE="qbittorrent"
+QBITTORRENT_DEPLOYMENT="qbittorrent"
+QBITTORRENT_CONTAINER="qbittorrent"
+GLUETUN_CONTAINER="gluetun"
+QBITTORRENT_URL="https://qbittorrent.dev-andrew.com"
+
+# Health thresholds
+GLUETUN_LOG_LOOKBACK="10m"
